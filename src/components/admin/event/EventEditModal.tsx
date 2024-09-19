9a86d0b9451b1df7d6ccc41728b270a42e294f7a
kauavitorrodrigues <kvrfz.prsnl@gmail.com>
2024-09-19 17:12:17 -0300

    add: create event edit modal layout

--- src/components/admin/event/EventEditModal.tsx
+++ src/components/admin/event/EventEditModal.tsx
@@ -1,7 +1,24 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { Event } from "@/types/Event"
-import { Edit } from "lucide-react"
+import { Edit, Save } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 type Props = {
     event: Event
@@ -19,21 +36,111 @@ export const EventEditModal = ({ event } : Props ) => {
                 </DropdownMenuItem>
             </DialogTrigger>
 
-            <DialogContent>
+            <DialogContent className="max-w-5xl" >
 
-                <DialogHeader>
+                <Tabs defaultValue="account" className="m-5">
 
-                    <DialogTitle>...</DialogTitle>
+                    <div className="flex justify-between w-full mb-4">
+                        <TabsList className="w-full">
+                            <TabsTrigger className="w-full" value="infos">Informações</TabsTrigger>
+                            <TabsTrigger className="w-full" value="groups">Grupos</TabsTrigger>
+                            <TabsTrigger className="w-full" value="people">Pessoas</TabsTrigger>
+                        </TabsList>
+                    </div>
 
-                    <DialogDescription>
-                        ...
-                    </DialogDescription>
+                    <TabsContent value="infos">
 
-                </DialogHeader>
+                        <Card>
+
+                            <CardHeader>
+
+                                <CardTitle>Informações</CardTitle>
+                                <CardDescription>
+                                    Atualize as informações gerais do evento.
+                                </CardDescription>
+                                
+                            </CardHeader>
+
+                            <CardContent className="space-y-2">
+                                
+                            </CardContent>
+
+                            <CardFooter>
+                                <Button 
+                                    className="w-full h-12 flex gap-2" 
+                                    onClick={() => {}}
+                                    > <Save size={20} /> Salvar
+                                </Button>
+                            </CardFooter>
+
+                        </Card>
+
+                    </TabsContent>
+
+                    <TabsContent value="groups">
+
+                        <Card>
+
+                            <CardHeader>
+
+                                <CardTitle>Grupos</CardTitle>
+                                <CardDescription>
+                                    Atualize as informações de grupo do evento.
+                                </CardDescription>
+                                
+                            </CardHeader>
+
+                            <CardContent 
+                                className="space-y-2">
+
+                            </CardContent>
+
+                            <CardFooter>
+                                <Button 
+                                    className="w-full h-12 flex gap-2" 
+                                    onClick={() => {}}
+                                    > <Save size={20} /> Salvar
+                                </Button>
+                            </CardFooter>
+
+                        </Card>
+
+                    </TabsContent>
+
+                    <TabsContent value="people">
+
+                        <Card>
+
+                            <CardHeader>
+
+                                <CardTitle>Pessoas</CardTitle>
+                                <CardDescription>
+                                    Atualize as informações de pessoas do evento.
+                                </CardDescription>
+                                
+                            </CardHeader>
+
+                            <CardContent className="space-y-2">
+                                
+                            </CardContent>
+
+                            <CardFooter>
+                                <Button 
+                                    className="w-full h-12 flex gap-2" 
+                                    onClick={() => {}}
+                                    > <Save size={20} /> Salvar
+                                </Button>
+                            </CardFooter>
+
+                        </Card>
+
+                    </TabsContent>
+
+                </Tabs>
 
             </DialogContent>
 
         </Dialog>        
     )
 
-}
+}   
